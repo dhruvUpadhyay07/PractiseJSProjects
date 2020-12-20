12ef5448605166c67fb679c9bcbd46175dc11116
Dhruv Upadhyay <dhruv.upadhyay@harness.io>
2020-12-20 20:30:51 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+const newYears = "25 December 2020";
+
+function countDown() {
+  const newYearsDate = new Date(newYears);
+  const currentDate = new Date();
+
+  const totalSeconds = (newYearsDate - currentDate) / 1000;
+  const days = Math.floor(totalSeconds / 3600 / 24);
+  const hours = Math.floor(totalSeconds / 3600) % 24;
+  const mins = Math.floor(totalSeconds / 60) % 60;
+  const seconds = Math.floor(totalSeconds) % 60;
+
+  const daysEl = document.getElementById("days");
+  const hoursEl = document.getElementById("hours");
+  const minsEl = document.getElementById("mins");
+  const secondsEl = document.getElementById("seconds");
+
+  daysEl.innerHTML = days;
+  hoursEl.innerHTML = formatTime(hours);
+  minsEl.innerHTML = formatTime(mins);
+  secondsEl.innerHTML = formatTime(seconds);
+}
+
+function formatTime(time) {
+  return time < 10 ? `0${time}` : time;
+}
+// initial call
+countDown();
+
+setInterval(countDown, 1000);
